22c2e29d42d44c771eb3c82990520706c47d710b
James Hughes <james.a.hughes@gmail.com>
2017-03-07 20:58:15 -0600

    added new products using seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product_list = [
+  [ "banana", 2, "banana_image", "This is a banana" ],
+  [ "nerf gun", 25, "nerf_gun_image", "This is a nerf gun"]
+]
+
+product_list.each do |product|
+  Product.create( :name => product[0], :price => product[1], :image => product[2], :description => product[3] )
+end

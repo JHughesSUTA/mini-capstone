1b626a82f21bfb42c1e23c765bc997edb7a218de
James Hughes <james.a.hughes@gmail.com>
2017-03-09 21:29:46 -0600

    add links between index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
 end

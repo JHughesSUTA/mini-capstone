187d1b23ce3d3c2f67ae6c53f7dba5730ad2ea9e
James Hughes <james.a.hughes@gmail.com>
2017-03-15 21:24:03 -0500

    change price to decimal and description to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308014836) do
+ActiveRecord::Schema.define(version: 20170316021855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.integer  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.string   "image"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
   end
 

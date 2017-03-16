8950e1d99245bdcec781afc9862082b6fabae94c
James Hughes <james.a.hughes@gmail.com>
2017-03-15 19:30:31 -0500

    add price, tax, and total to show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+
+  def sale_message 
+    if price.to_f < 100
+      "Discount Item!"
+    else
+      "Everyday Value!!"
+    end
+  end
+
+  def tax
+    tax = (price.to_f * 0.09).round(2)
+  end
+
+  def total
+    total = price.to_f + tax
+  end
+
 end

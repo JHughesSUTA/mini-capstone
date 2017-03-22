cde848bbacee8d672e2abe0eee513f7e91dec62b
James Hughes <james.a.hughes@gmail.com>
2017-03-21 19:41:12 -0500

    Add multiple images from images table to show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321231045) do
+ActiveRecord::Schema.define(version: 20170321232525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20170321231045) do
 
   create_table "products", force: :cascade do |t|
     t.decimal  "price",       precision: 8, scale: 2
-    t.string   "image"
     t.text     "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false

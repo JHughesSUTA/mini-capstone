9431667880a6a7453b2867aeb9e4be1b11db0bc8
James Hughes <james.a.hughes@gmail.com>
2017-03-28 19:53:26 -0500

    create action of carted products controller, save user_id, product_id, quantity and status

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,13 @@
 class CartedProductsController < ApplicationController
   def create
+    carted_product = CartedProduct.new(
+      quantity: params[:quantity], 
+      product_id: params["product_id"],
+      user_id: current_user.id,
+      status: "carted"
+    )
+    # order.calculate_totals
+    carted_product.save
+    redirect_to "/carted_products"
   end
 end
